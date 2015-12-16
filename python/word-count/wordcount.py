2b37aa569f67ab4fbba330d87d3f5f9986b5794d
Rick Ertl <ertl.rick@gmail.com>
2015-12-15 20:22:44 -0600

    1st iteration of OK response

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,1 +1,6 @@
-wordcount.py
+def word_count(phrase):
+	word_dict = {}
+	split_phrase = phrase.lower().split()
+	for word in split_phrase:
+		word_dict.update({word: split_phrase.count(word)})
+	return(word_dict)
